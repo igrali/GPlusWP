00a271066198e5a8a7bbc04f2d810795aab47e7e
igrali <igor.ralic@outlook.com>
2013-09-01 20:04:11 +0200

    Initial project commit

---
+++ GooglePlusDemoWP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GooglePlusDemoWP.Resources;
+
+namespace GooglePlusDemoWP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
